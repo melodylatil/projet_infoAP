bf5070c5298778279f06a6452051b71830490df7
Mélody Latil <melody.latil@etu.minesparis.psl.eu>
2026-01-21 16:26:34 +0100

    correction erreurs tableau.py

--- tableau.py
+++ tableau.py
@@ -13,7 +13,7 @@ class Framework:
                 print("[]", end="")
             print()
 
-    def remplir(self, tableau, position, couleur, pourcentage,nb_personnes):
+    def remplir(self, position, couleur, pourcentage,nb_personnes):
         #remplit une case du tableau avec une couleur donnée
         liste_positions = []        
         for i in range(self.nb_cases):
@@ -25,19 +25,19 @@ class Framework:
             x = liste_positions[numero][0]
             y = liste_positions[numero][1]
             liste_positions.pop(numero)
-            tableau[x][y] = "B"
+            self[x][y] = "B"
 
         for k in range(nb_personnes - n_B):
             numero = rd.randint(0, len(liste_positions)-1)
             x = liste_positions[numero][0]  
             y = liste_positions[numero][1]
             liste_positions.pop(numero)
-            tableau[x][y] = "R"
+            self[x][y] = "R"
 
-    def vide(self, tableau):
+    def vide(self):
         #renvoie la liste des cases non occupées du tableau
         liste_cases_vides = []  
         for i in range(self.nb_cases):
             for j in range(self.nb_cases):
-                if tableau[i][j] == 0:
+                if self[i][j] == 0:
                     liste_cases_vides.append((i, j))
